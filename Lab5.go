b16525eec24e1ef27b3e124ae89dfb6980658a37
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-07-22 12:41:32 +0530

    Create Lab5.go

---
+++ Lab5.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func add(i int) time.Time {
+	fmt.Println(i)
+	x:= time.Now()
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("in add", i ," time = " , x)
+	return  x
+}
+func helper1(){
+	for i :=1;i<5;i++{
+		defer add(i)
+		fmt.Println("cccc",i)
+	}
+	add(1)
+	add(2)
+	add(3)
+}
+
+
+func main() {
+
+	helper1()
+}
