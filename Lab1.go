6d6aec53015a31cd64bb9f794c7e4a662a93c32d
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-07-22 10:38:02 +0530

    Create Lab1.go

---
+++ Lab1.go
@@ -0,0 +1,21 @@
+fmt.Println("Hello World")
+	//var c1=0
+	//c1 := 0
+	var c1 int
+	c1 = 10
+	fmt.Println(c1)
+	/*var a = 0
+	var b = 1.23
+	var c = false
+	*/
+
+	var a, b, c = 0, 1.23, false
+	// a = b -> conversion error
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	
+	c1,err= fmt.Scan()
+	if (err)
+		
+	fmt.Println(c1)
