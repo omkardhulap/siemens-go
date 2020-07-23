986980dda6aae03fe1b75a2c550e522db30e32a5
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-07-23 12:24:31 +0530

    Update Lab5.go

--- Lab5.go
+++ Lab5.go
@@ -2,28 +2,14 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-
-func add(i int) time.Time {
-	fmt.Println(i)
-	x:= time.Now()
-	time.Sleep(1000 * time.Millisecond)
-	fmt.Println("in add", i ," time = " , x)
-	return  x
-}
-func helper1(){
-	for i :=1;i<5;i++{
-		defer add(i)
-		fmt.Println("cccc",i)
-	}
-	
-}
-
-
 func main() {
-
-	helper1()
+	fmt.Println("Number please")
+	empno := 0
+	ename := ""
+	fmt.Println("Current Value of empno ", empno)
+	no, err := fmt.Scanf("%d", &empno)
+	fmt.Println("Current Value of empno ", empno, " , "+ename)
+	fmt.Println(no, "  ", err)
 }
-
