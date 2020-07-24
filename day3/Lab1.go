2c0156c6586afcc064a63824fc634ff9ad29766f
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-07-24 10:25:19 +0530

    Add files via upload

--- day3/Lab1.go
+++ day3/Lab1.go
@@ -10,11 +10,11 @@ func main() {
 	fmt.Println(primes)
 	var s []int = primes[:]
 	fmt.Println("s with :", s)
-	s = primes[5:7]
-	fmt.Println("s with 5:7", s)
+	s = primes[3:7]
+	fmt.Println("s with 3:7", s)
 	primes[3] = 99
 	s[1] = 88
-	fmt.Println("s with 5:7", s)
+	fmt.Println("s with 3:7", s)
 	fmt.Println("Prime:   ", primes)
 	fmt.Println("slice len = ", len(s), ", cap = ", cap(s))
 	fmt.Println("Array len = ", len(primes), ", cap = ", cap(primes))
