0bb6cf1c58077f381c5166c447552aec9dd73642
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-07-24 10:01:37 +0530

    Add files via upload

---
+++ day3/Lab1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello")
+	primes := [10]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+	var s []int = primes[:]
+	fmt.Println("s with :", s)
+	s = primes[5:7]
+	fmt.Println("s with 5:7", s)
+	primes[3] = 99
+	s[1] = 88
+	fmt.Println("s with 5:7", s)
+	fmt.Println("Prime:   ", primes)
+	fmt.Println("slice len = ", len(s), ", cap = ", cap(s))
+	fmt.Println("Array len = ", len(primes), ", cap = ", cap(primes))
+
+}
