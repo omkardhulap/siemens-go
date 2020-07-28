ae20aea23efb5f138b7842513b019baffad9199d
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-07-28 11:14:46 +0530

    Add files via upload

--- day5/EmpHandler.go
+++ day5/EmpHandler.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
+
+type EmpStruct struct {
+	Empno  int     `json:"empno"`
+	Ename  string  `json:"ename"`
+	Salary float64 `json:"salary"`
+}
 
 func Emphandler(w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case "GET":
 		io.WriteString(w, "<h1>GET</h1>")
 	case "POST":
-	  	  io.WriteString(w, "<h1>EmpHandler POST Page</h1>")
-	    }
+		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", req.PostForm)
+		reqBody, _ := ioutil.ReadAll(req.Body)
+		var emp EmpStruct
+		json.Unmarshal(reqBody, &emp)
+		Save(emp)
+		json.NewEncoder(w).Encode(emp)
+	}
 }
