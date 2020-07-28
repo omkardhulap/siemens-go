9dc604c9e45bd6f49e145000a041b1ac0cf40e17
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-07-28 13:27:00 +0530

    Add files via upload

--- testing/Lab1.go
+++ testing/Lab1.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func add(i int, j int) int {
 	return i + j
 }
+func divide(s string, s1 string) int {
+	n1, _ := strconv.Atoi(s)
+	n2, _ := strconv.Atoi(s1)
+	return n1 / n2
+}
 func main() {
 	x := add(20, 30)
 	fmt.Println("x = ", x)
+	fmt.Println("div = ", divide("10", "5"))
 }
--- testing/Lab1_test.go
+++ testing/Lab1_test.go
@@ -2,9 +2,15 @@ package main
 
 import "testing"
 
-func TestAdd1(t *testing.T) {
+func TestAdd(t *testing.T) {
 	x := add(10, 30)
+
 	if x != 40 {
 		t.Error("Invalid Sum")
+	}
+}
+func BenchmarkAdd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = add(i*10, i*30)
 	}
 }
